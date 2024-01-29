913e700ad232093c185996258483e3806f41b8cd
dpk6691 <dip6691@gmail.com>
2024-01-29 11:05:52 +0530

    Update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,62 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-}
+  extends: ["react-app", "eslint:recommended", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      pragma: "React",
+      version: "16.8",
+    },
+  },
+  plugins: ["react"],
+  rules: {
+    quotes: [
+      "error",
+      "single",
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: "off",
+    "default-case": [
+      "error",
+      {
+        commentPattern: "^no default$",
+      },
+    ],
+    "no-new-wrappers": 0,
+    "no-mixed-operators": 0,
+    "require-atomic-updates": "off",
+    "comma-dangle": "off",
+    "no-unused-vars": "off",
+    "no-useless-constructor": 0,
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/no-unescaped-entities": 0,
+    "react/display-name": 0,
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "no-useless-escape": 0,
+    "no-console": 0,
+    "no-debugger": 0,
+    "no-empty": 0,
+    "linebreak-style": 0,
+    "import/first": 0,
+    "import/imports-first": 0,
+    "no-shadow": 0,
+    "disable-next-line": 0,
+    "no-case-declarations": 0,
+  },
+};
